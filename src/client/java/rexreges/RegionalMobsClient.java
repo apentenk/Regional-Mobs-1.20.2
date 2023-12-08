52f5174c8fde417add94ce40b821f371e461635f
Kwasi <kwasi.a.apenteng@gmail.com>
2023-12-08 09:10:03 -0500

    Initialized development environment

---
+++ src/client/java/rexreges/RegionalMobsClient.java
@@ -0,0 +1,10 @@
+package rexreges;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RegionalMobsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
